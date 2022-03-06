6180e19cbaf1822d14aaf54b47ebbb7374104165
PcBoyarin <92618851+SeemerGG@users.noreply.github.com>
2022-03-06 14:51:41 +0300

    Задания 11 и 12

---
+++ DZ_LAB_5/DZ_LAB_5/Program.fs
@@ -0,0 +1,19 @@
+﻿//№11
+(*let input=
+    let a = System.Console.ReadLine()
+    if ((a = "Prolog") ||(a = "F#" )) then System.Console.WriteLine("Подлиза")
+    else System.Console.WriteLine("Харош");;*)
+//№12
+[<EntryPoint>]
+//#12.1 
+(*let main argv =
+    match System.Console.ReadLine() with
+    |"Prolog"->System.Console.WriteLine("Подлиза")
+    |"F#"->System.Console.WriteLine("Подлиза")
+    | _->System.Console.WriteLine("ХАРОШ")
+    *)
+//#12.2
+(*let main arvg=
+    System.Console.WriteLine(System.Console.ReadLine() |> (fun x -> if (x = "Prolog")||(x = "F#") then "Подлиза" else "ХАРОШ"))
+    *)
+    0 // return an integer exit code
