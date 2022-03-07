44c0bf6469cc2c2e187ad9476a80c643c923ae7a
PcBoyarin <92618851+SeemerGG@users.noreply.github.com>
2022-03-07 17:04:50 +0300

    13

--- DZ_LAB_5/DZ_LAB_5/Program.fs
+++ DZ_LAB_5/DZ_LAB_5/Program.fs
@@ -1,9 +1,68 @@
 ﻿//№11
 (*let input=
     let a = System.Console.ReadLine()
-    if ((a = "Prolog") ||(a = "F#" )) then System.Console.WriteLine("Подлиза")
+    if ((a = "Prolog") || (a = "F#" )) then System.Console.WriteLine("Подлиза")
     else System.Console.WriteLine("Харош");;*)
-//№12
+//#13
+(*
+let mult n=
+    match n with
+    |0->0
+    | _->
+        let rec mult_c1 n=
+            if n = 0 then 1
+            else (n % 10) * mult_c1 (n / 10)
+        mult_c1 n
+
+let mult_c_t n=
+    match n with
+    |0->0
+    | _->
+    let rec mult_c_t1 acc n=
+        if n = 0 then acc
+        else mult_c_t1 (acc * (n % 10)) (n / 10)
+    mult_c_t1 1 n
+
+let min_c_t n=
+    let rec min_c1 c d max=
+        let new_d= c % 10
+        let new_max= if max<new_d then max else new_d
+        if c=0 then max
+        else min_c1 (c / 10) new_d new_max
+    min_c1 n 0 9
+
+let min_c n=
+    let rec create_list n list=
+       if n = 0 then list 
+       else create_list (n / 10) ((n % 10)::list)
+    List.min (create_list n [])
+        
+let max_c_t n=
+    let rec max_c1 c d max=
+        let new_d= c % 10
+        let new_max= if max>new_d then max else new_d
+        if c=0 then max
+        else max_c1 (c / 10) new_d new_max
+    max_c1 n 0 0  
+     
+let max_c n=
+    let rec create_list n list=
+       if n = 0 then list 
+       else create_list (n / 10) ((n % 10)::list)
+    List.max (create_list n [])
+
+let output=
+    let n=System.Convert.ToInt32(System.Console.ReadLine()) in
+    System.Console.WriteLine(mult n)
+    System.Console.WriteLine(mult_c_t n)
+    System.Console.WriteLine(max_c_t  n)
+    System.Console.WriteLine(min_c_t  n)
+    System.Console.WriteLine(min_c  n)
+    System.Console.WriteLine(max_c  n);;
+*)
+
+
+    
 [<EntryPoint>]
 //#12.1 
 (*let main argv =
