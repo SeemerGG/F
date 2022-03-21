dc08fbcd09e3f44f32c286fce8325056582163cd
PcBoyarin <92618851+SeemerGG@users.noreply.github.com>
2022-03-21 09:24:34 +0300

    Создание проекта

---
+++ DZ_LAB_6/DZ_LAB_6/Program.fs
@@ -0,0 +1,16 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+let rec input_list n=
+    if n = 0 then []
+    else 
+        let head = Convert.ToInt32(Console.ReadLine())
+        let tail = input_list (n - 1)
+        head::tail
+    
+//#7
+let 
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
