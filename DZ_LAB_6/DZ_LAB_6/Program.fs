569598f0cd8158dd002d22265ba0404ac3aa84df
PcBoyarin <92618851+SeemerGG@users.noreply.github.com>
2022-03-21 19:43:33 +0300

    Реализация задачи №17

--- DZ_LAB_6/DZ_LAB_6/Program.fs
+++ DZ_LAB_6/DZ_LAB_6/Program.fs
@@ -29,6 +29,30 @@ let rec diffrent_elem f list =
     |h::t -> if f h then h else diffrent_elem f t 
     |[] -> 0
 
+let min_elem list =
+    let rec min list acc=
+        match list with
+        |h::t -> if h < acc then min t h else min t acc
+        |[] -> acc
+    min list (List.head list)
+
+let max_elem list =
+    let rec max list acc=
+        match list with
+        |h::t -> if h > acc then max t h else max t acc
+        |[] -> acc
+    max list (List.head list)
+
+let rec flip list newlist=
+    match list with 
+    |h::t -> flip t (h::newlist)
+    |[] -> newlist
+
+let rec swap list x y new_list=
+    match list with
+    |h::t -> if h = x  then swap t x y (y::new_list) else if h = y then swap t x y (x::new_list) else swap t x y (h::new_list)
+    |[] -> flip new_list []
+
 let zad11 list =
     let elem1 = List.head list
     let elem2 = diffrent_elem (fun x -> x <> elem1) list
@@ -36,10 +60,16 @@ let zad11 list =
     let acc2 = count (fun x -> x = elem2) list 0
     if acc1 > acc2 then Console.WriteLine("Элемент встречаемый единожды {0}", elem2) else Console.WriteLine("Элемент встречаемый единожды {0}", elem1) 
 
+let zad17 list =
+    let max = max_elem list
+    let min = min_elem list
+    swap list min max []
+
+
 [<EntryPoint>]
 let main argv =
     let list = read_list 
     match Console.ReadLine() with
     |"11" -> zad11 <| list
-
+    |"17" -> list |> zad17 |> output_list |> ignore
     0 // return an integer exit code
