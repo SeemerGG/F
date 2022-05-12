31e22cd8f2e49f447fb7f2b1e6d84234422232e8
PcBoyarin <92618851+SeemerGG@users.noreply.github.com>
2022-05-12 20:05:48 +0300

    Задание 18.13

--- Prolog/Lab_13.pl
+++ Prolog/Lab_13.pl
@@ -146,6 +146,26 @@ near(A,B,T):-right(A,B,T).
 near(A,B,T):-left(A,B,T).
 
 
+% Задание 18
+
+zad18:-Men=[_,_,_,_],
+	in_list(Men,[voronov,_]),
+	in_list(Men,[pavlov,_]),
+	in_list(Men,[levitskiy,_]),
+	in_list(Men,[sahorov,_]),
+	in_list(Men,[_,dancer]),
+	in_list(Men,[_,artist]),
+	in_list(Men,[_,writer]),
+	in_list(Men,[_,singer]),
+	not(in_list(Men,[voronov,singer])),
+	not(in_list(Men,[levitskiy,singer])),
+	not(in_list(Men,[pavlov,writer])),
+	not(in_list(Men,[pavlov,artist])),
+	not(in_list(Men,[voronov,writer])),
+	not(in_list(Men,[sahorov,writer])),
+	write(Men),!.
+
+
 
 
 
