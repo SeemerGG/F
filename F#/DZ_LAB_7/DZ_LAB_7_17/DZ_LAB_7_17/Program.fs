815de92a810efbcbe827ea3a4f7c70bc6cec22de
PcBoyarin <92618851+SeemerGG@users.noreply.github.com>
2022-05-25 00:30:13 +0300

    F# Задание 17(7).7

--- F#/DZ_LAB_7/DZ_LAB_7_17/DZ_LAB_7_17/Program.fs
+++ F#/DZ_LAB_7/DZ_LAB_7_17/DZ_LAB_7_17/Program.fs
@@ -12,17 +12,23 @@ let rec output_list (list:int list) =
     |[] -> None
     |h::t-> Console.Write("{0} ", h)
             output_list t
-
-let del_ch n avg list =
-    List.forall (fun x -> if (snd x % n = 0) && ((fst x % 2 = 0) || ((x<avg) && (snd x % n=0))) then false else true) list
-
-let del_list n list=
+    
+let del_ch (n:float) (avg:float) list_i list =
+    if List.exists(fun x -> (snd x % n = 0.0) && (fst x % 2 = 0)) list_i && List.forall(fun x -> if (x<avg) && (x % n = 0) then false else true) list then true else false
+    
+let del_list_sum n (list:float list)=
     let avg = List.average list
     let rec obhod (i:int) (sum:int) =
         if n < i then sum
-        else (if (n % i = 0) && (del_ch i avg (List.indexed list)) then obhod (i + 1) (sum + i) else obhod (i + 1) sum)
+        else (if (n % i = 0) && (del_ch i avg (List.indexed list) list) then obhod (i + 1) (sum + i) else obhod (i + 1) sum)
     obhod 2 0
 
 let zad list =
-    let i_list = List.indexed list
-    List.sortBy (fun x -> List.fold (fun sum y -> ) 0 i_list) 
+    let list_float = List.map(fun x  -> float x) list
+    List.sortBy (fun x -> del_list_sum x list_float) list
+   
+[<EntryPoint>]
+let main argv =
+    let list = input_list (Convert.ToInt32(Console.ReadLine()))
+    list |> zad |> output_list |> ignore
+    0
