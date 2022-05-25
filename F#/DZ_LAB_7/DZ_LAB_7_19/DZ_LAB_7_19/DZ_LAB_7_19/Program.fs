80b32333583f83f1693fd1aa207b7f1e7e9b4dd8
PcBoyarin <92618851+SeemerGG@users.noreply.github.com>
2022-05-25 18:26:32 +0300

    F# Задание 19.2.7

--- F#/DZ_LAB_7/DZ_LAB_7_19/DZ_LAB_7_19/DZ_LAB_7_19/Program.fs
+++ F#/DZ_LAB_7/DZ_LAB_7_19/DZ_LAB_7_19/DZ_LAB_7_19/Program.fs
@@ -1,4 +1,5 @@
 ﻿open System
+open System.Text.RegularExpressions
 
 let zad1 (str:string) =
      String.length(String.filter(fun x -> x>='А' && x<='я') str)
@@ -7,6 +8,10 @@ let zad2 (str:string) =
     let str_flip = Seq.rev str
     Seq.forall2 (fun x y -> x=y) str str_flip
 
+let zad3 (str: string[]) = 
+    let new_str = Array.filter (fun x -> Regex.IsMatch(x,@"(0[1-9]|[12][0-9]|3[01])[.](01|03|05|07|08|10|12)[.](19|20)\d\d") || 
+Regex.IsMatch(x,@"(0[1-9]|[1][0-9]|2[0-8])[.](02)[.](19|20)\d\d") || Regex.IsMatch(x,@"(0[1-9]|[12][0-9]|30)[.](04|06|09|11)[.](19|20)\d\d")) str
+    Array.map (fun x -> printf "%s " x) new_str 
 
 [<EntryPoint>]
 let main argv = 
@@ -19,5 +24,6 @@ let main argv =
             Console.WriteLine("Количество русских символов: {0}",str |> zad1)
     |"2" -> let str = Console.ReadLine()
             if str |> zad2 then Console.WriteLine("Да") else Console.WriteLine("Нет")
-
+    |"3" -> let str = Console.ReadLine()
+            str.Split(' ') |> zad3 |> ignore 
     0
