6ba48e1c5cfb871733181e3aeef9de694d86b185
PcBoyarin <92618851+SeemerGG@users.noreply.github.com>
2022-05-25 08:21:14 +0300

    F# Задание 18.7

---
+++ F#/DZ_LAB_7/DZ_LAB_7_18/DZ_LAB_7_18/DZ_LAB_7_18/Program.fs
@@ -0,0 +1,23 @@
+﻿open System
+
+let read_array n =
+    let rec read_array_r n arr = 
+        if n = 0 then
+            arr
+        else
+            let tail = System.Console.ReadLine() |> Int32.Parse
+            let new_arr = Array.append arr [|tail|]
+            let n1 = n - 1
+            read_array_r n1 new_arr
+
+    read_array_r n Array.empty
+
+let write_array arr =
+    printfn "%A" arr
+
+[<EntryPoint>]
+let main argv =
+    let arr1 = read_array (Console.ReadLine() |> Int32.Parse)
+    let arr2 = read_array (Console.ReadLine() |> Int32.Parse)
+    arr1 |> Array.filter (fun x -> Array.exists (fun y -> y = x) arr2 ) |> write_array
+    0
